5bf5a3c3da9b244b2562a905256e3431067b0fe4
Candy <candy@CandydeiMac-Pro.local>
2020-09-06 16:53:23 +0800

    delete user

--- microservice-simple-provider-user/src/main/java/com/microservice/microservicesimpleprovideruser/Dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.microservicesimpleprovideruser.Dao;
-import com.microservice.microservicesimpleprovideruser.entity.User;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public  interface UserMapper {
-    User getUserById(String id);
-}
