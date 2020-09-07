8194a6b067482646cc3ba03ee5516ba9eb198595
Candy <candy@CandydeiMac-Pro.local>
2020-09-07 12:04:59 +0800

    Test update

--- microservice-simple-provider-user/src/test/AppTest.java
+++ microservice-simple-provider-user/src/test/AppTest.java
@@ -1,4 +1,4 @@
-package com.microservice.microservicesimpleprovideruser;
+package src.main;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
